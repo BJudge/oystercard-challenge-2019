ef2b925060d26d6b142684a8a007c3763aadafc2
BartJudge <BartJudge@gmail.com>
2019-01-23 17:56:17 +0000

    setting a maximum balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,7 +3,14 @@ describe Oystercard do
   it 'should have a balance of £0' do
       expect(subject.balance).to eq 0
   end
-  it 'should be able to top_up' do
-    expect{subject.top_up 5}.to change {subject.balance}.by 5
+  describe "#top_up" do
+    it 'should be able to top_up' do
+      expect{subject.top_up 5}.to change {subject.balance}.by 5
+    end
+    it 'should have a limit of £90' do
+      subject.top_up(5)
+      expect{subject.top_up(86)}.to raise_error "cannot exceed balance of #{Oystercard::MAXIMUM_BALANCE}"
+    end
   end
+
 end
